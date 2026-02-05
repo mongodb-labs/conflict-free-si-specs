cc648e08e9f789134bdfe343aeca82ef9e48616e
William Schultz <william.schultz@mongodb.com>
2026-02-05 15:58:42 -0500

    Add seed arg

--- check.sh
+++ check.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-tlc -simulate -deadlock -dumpTrace json traces/trace.json -depth 25 -workers 10 SnapshotIsolation | tee logout
+
+depth=18
+if [ -n "$1" ]; then
+    tlc -simulate -deadlock -dumpTrace json traces/trace.json -depth $depth -workers 10 -seed "$1" SnapshotIsolation | tee logout
+else
+    tlc -simulate -deadlock -dumpTrace json traces/trace.json -depth $depth -workers 10 SnapshotIsolation | tee logout
+fi
 python3 viz.py traces/trace.json
 python3 tla_to_transaction_history.py traces/trace.json
